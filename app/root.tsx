0f4a6dafd79586cb4b56b4bf4891fda871d5928f
Roman <romanyunko1996@gmail.com>
2025-10-21 22:43:15 +0300

    Integrates Puter SDK and establishes core authentication flow
    Initializes the Puter SDK for platform integration and user authentication. Adds a dedicated authentication route and updates the home page to enforce authentication, redirecting unauthenticated users. Refreshes the home page UI to introduce a navigation bar and display resume tracking capabilities.

--- app/root.tsx
+++ app/root.tsx
@@ -7,11 +7,13 @@ import {
   ScrollRestoration,
 } from "react-router";
 
-import type { Route } from "./+types/root";
+import type {Route} from "./+types/root";
 import "./app.css";
+import {usePuterStore} from "./lib/puter";
+import { useEffect } from "react";
 
 export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {rel: "preconnect", href: "https://fonts.googleapis.com"},
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
@@ -23,7 +25,13 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({children}: {children: React.ReactNode}) {
+  const {init} = usePuterStore();
+
+  useEffect(() => {
+    init();
+  }, [init]); 
+  
   return (
     <html lang="en">
       <head>
@@ -33,6 +41,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <script src="https://js.puter.com/v2/"></script>
+
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -45,7 +55,7 @@ export default function App() {
   return <Outlet />;
 }
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+export function ErrorBoundary({error}: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
   let stack: string | undefined;
